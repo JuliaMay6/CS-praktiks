b0e513048935c6a8160f6fadc1fec0e0b096e87d
Julia <student@C426U28>
2020-09-08 14:07:38 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pr1_z1
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+
+    }
+    public string firstName;
+    public string lastName;
+    public int age;
+  }
+  public override string ToString()
+  {
+    return firstName + " " + lastName + ", age " + age;
+  }
+  // C#
+  Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
+
+  static void Main(string[] args)
+    
+  
+  }
+}
